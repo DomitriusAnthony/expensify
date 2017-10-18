1d566db3e03f6fd0eb86e5953f34827321f6779b
Domitrius Clark <domitrius.anthony@hotmail.com>
2017-10-18 04:28:06 -0400

    Added ability to choose a date to add an expense

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import ExpenseForm from './ExpenseForm';
 
 const AddExpensePage = () => (
     <div>
-        This is from my add expense component.
+        <h1>Add Expense</h1>
+        <ExpenseForm 
+            onSubmit={(expense) => {
+                console.log(expense);
+            }}
+        />
     </div>
 );
 
